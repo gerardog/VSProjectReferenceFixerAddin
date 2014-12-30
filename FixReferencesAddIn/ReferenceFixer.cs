42bef0717367bcb831abb15a74a4b16f04da0fb4
Gerardo Grignoli <gerardog@gmail.com>
2014-12-30 15:01:15 -0300

    Initial version.

---
+++ FixReferencesAddIn/ReferenceFixer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE80;
+using EnvDTE;
+
+namespace FixReferencesAddIn
+{
+    class ReferenceFixer
+    {
+        private DTE2 _applicationObject;
+        private AddIn _addInInstance;
+        OutputWindow outputWindow;
+
+        public ReferenceFixer(DTE2 _applicationObject, AddIn _addInInstance)
+        {
+            this._addInInstance = _addInInstance;
+            this._applicationObject = _applicationObject;
+
+            Window window = _applicationObject.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
+            outputWindow = (OutputWindow)window.Object;
+            outputWindow.ActivePane.Activate();
+        }
+
+        public void Run()
+        {
+            var projects = _applicationObject.Solution.Projects;
+            Dictionary<string, Project> outputs = GetProjectsOutputs(projects);
+
+            foreach (Project p in projects)
+            {
+                var proj = p.Object as VSLangProj.VSProject;
+
+                if (proj != null)
+                {
+                    for (int i = 1; i <= proj.References.Count; i++)
+                    {
+                        var r = proj.References.Item(i);
+
+                        if (r.SourceProject == null && outputs.ContainsKey(r.Name))
+                        {
+                            var referenceName = r.Name; // must cache before removing reference
+                            WriteLine(" Fixing References on project {0} for project {1}", p.Name, outputs[r.Name].Name);
+
+                            r.Remove();
+                            i = 0; // reset i to start over.
+
+                            proj.References.AddProject(outputs[referenceName]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private Dictionary<string, Project> GetProjectsOutputs(Projects projects)
+        {
+            var result = new Dictionary<string, Project>( StringComparer.OrdinalIgnoreCase  );
+
+            foreach (Project p in projects)
+            {
+                var proj = p.Object as VSLangProj.VSProject;
+
+                if (proj != null)
+                {
+                    var r = p.Properties.Item("OutputFileName");
+                    if (r != null)
+                    {   
+                        result.Add(r.Value.ToString().Replace(".dll",""), p);
+                        WriteLine(" Project {0} generates assembly: {1}", p.Name, r.Value.ToString().Replace(".dll",""));
+                    }
+                }
+            }
+            return result;
+        }
+
+
+        void WriteLine(string text, params object[] parameters)
+        {
+            outputWindow.ActivePane.OutputString(string.Format(text + "\n", parameters));
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,25 @@ VSProjectReferenceFixerAddin
 ============================
 
 A Visual Studio Add In that looks for assembly references between projects and replaces them with proper Project References
+
+Did you messed up when adding the references to a project and used assembly references instead of project references?
+
+
+Installation
+------------
+Create folder %APPDATA%\Microsoft\MsEnvShared\Addins
+Download release.ZIP and unzip there.
+Restart Visual Studio
+
+Notes:
+
+How to use
+----------
+In visual studio, Go to Tools -> FixReferencesAddIn
+
+Look at the results in the Output window. For Example:
+```
+ Project a generates assembly: a
+ Project b generates assembly: b
+ Fixing References on project b for project a
+```
